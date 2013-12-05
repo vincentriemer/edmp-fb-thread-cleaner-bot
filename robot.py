5fa54dafc5e9dd0bf4c34e23de99cc35b462d589
Vincent Riemer <vincentriemer@gmail.com>
2013-12-05 17:49:32 -0500

    fixed imports

--- robot.py
+++ robot.py
@@ -20,7 +20,8 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 # THE SOFTWARE.
 
-import praw, time
+import praw, time, ConfigParser
+from optparse import OptionParser
 
 USERAGENT = "Edmproduction Feedback Thread Cleaner | a bot by /u/BizCaus"
 
